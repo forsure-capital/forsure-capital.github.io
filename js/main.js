a67993ebed2d0a7c0cfd077e593ca8f68197ee10
Sergio Esteban <sergio.smiling@gmail.com>
2020-02-22 20:08:42 -0500

    New DESIGN

--- js/main.js
+++ js/main.js
@@ -112,6 +112,24 @@
 	};
 	siteScroll();
 
+	//Intro carousel
+	var heroCarousel = $("#heroCarousel");
+	var heroCarouselIndicators = $("#hero-carousel-indicators");
+	heroCarousel.find(".carousel-inner").children(".carousel-item").each(function (index) {
+		(index === 0) ?
+			heroCarouselIndicators.append("<li data-target='#heroCarousel' data-slide-to='" + index + "' class='active'></li>") :
+			heroCarouselIndicators.append("<li data-target='#heroCarousel' data-slide-to='" + index + "'></li>");
+
+		$(this).css("background-image", "url('" + $(this).children('.carousel-background').children('img').attr('src') + "')");
+		$(this).children('.carousel-background').remove();
+	});
+
+	heroCarousel.on('slid.bs.carousel', function (e) {
+		$(this).find('h2').addClass('animated fadeInDown');
+		$(this).find('p').addClass('animated fadeInUp');
+		$(this).find('.btn-get-started').addClass('animated fadeInUp');
+	});
+
 	var siteSticky = function () {
 		$(".js-sticky-header").sticky({ topSpacing: 0 });
 	};

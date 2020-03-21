04efe7b1444eb8097b147b13763fb82c946be463
Sergio Esteban <sergio.smiling@gmail.com>
2020-03-20 20:28:03 -0500

    new video3

--- js/main.js
+++ js/main.js
@@ -82,6 +82,24 @@
 			}
 		})
 
+		// Clients carousel (uses the Owl Carousel library)
+		$(".clients-carousel").owlCarousel({
+			autoplay: true,
+			dots: true,
+			loop: true,
+			responsive: {
+				0: {
+					items: 2
+				},
+				768: {
+					items: 4
+				},
+				900: {
+					items: 6
+				}
+			}
+		});
+
 		// click outisde offcanvas
 		$(document).mouseup(function (e) {
 			var container = $(".site-mobile-menu");
